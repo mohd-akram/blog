a7ef36e3246a2ef0798498acf648ad2eb938a9f6
Mohamed Akram <mohd.akram@outlook.com>
2016-09-01 00:27:53 +0400

    Update Pelican theme

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-THEME = 'themes/pelican-bootstrap3'
+THEME = 'themes/pelican-themes/pelican-bootstrap3'
 
 AUTHOR = 'Mohamed Akram'
 SITENAME = 'Dev Blog'
